f90d69333f6b78b025e5c2b1a9a5bd69ee57c85c
Kamil Rosinski <kamilrosiczka@gmail.com>
2019-09-09 20:10:46 +0200

    missing file

--- snake-client/src/main/webapp/src/app/app.module.ts
+++ snake-client/src/main/webapp/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SnakeComponent } from './snake/snake.component';
-import { MessagesComponent } from './messages/messages.component';
+import {AppComponent} from './app.component';
+import {SnakeComponent} from './snake/snake.component';
+import {MessagesComponent} from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SnakeComponent,
-    MessagesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SnakeComponent,
+        MessagesComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
