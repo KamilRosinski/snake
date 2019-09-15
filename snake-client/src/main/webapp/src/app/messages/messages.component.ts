f79e0ab545efeed5dcce6f2c3a6269508e42f175
Kamil Rosinski <kamilrosiczka@gmail.com>
2019-09-15 15:37:25 +0200

    messages moved into table; sorting by timestamp implemented

--- snake-client/src/main/webapp/src/app/messages/messages.component.ts
+++ snake-client/src/main/webapp/src/app/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Message} from "./message";
 import {MessagingService} from "./messaging.service";
+import {SortOrder} from "./sort-order";
 
 @Component({
     selector: 'app-messages',
@@ -9,17 +10,34 @@ import {MessagingService} from "./messaging.service";
 })
 export class MessagesComponent implements OnInit {
 
-    messages: Array<Message> = [];
+    private _messages: Array<Message> = [];
+    private _order: SortOrder = SortOrder.ASCENDING;
 
     constructor(private messagingService: MessagingService) {
     }
 
-    ngOnInit() {
-        this.messagingService.getMessenger().subscribe(message => this.messages.push(message));
+    ngOnInit(): void {
+        this.messagingService.getMessenger().subscribe(message => this._messages.push(message));
     }
 
-    clearMessages() {
-      this.messages = [];
+    clearMessages(): void {
+      this._messages = [];
+    }
+
+    toggleSortOrder(): void {
+        this._order = this._order === SortOrder.ASCENDING ? SortOrder.DESCENDING : SortOrder.ASCENDING;
+    }
+
+    get messagesLength(): number {
+        return this._messages.length;
+    }
+
+    get sortedMessages(): Array<Message> {
+        return this._messages.sort((m1, m2) => this.compareTimestamps(m1.timestamp.getTime(), m2.timestamp.getTime()));
+    }
+
+    private compareTimestamps(timestamp1: number, timestamp2: number): number {
+        return this._order === SortOrder.ASCENDING ? timestamp1 - timestamp2 : timestamp2 - timestamp1;
     }
 
 }
---
+++ snake-client/src/main/webapp/src/app/messages/sort-order.ts
@@ -0,0 +1,6 @@
+export enum SortOrder {
+
+    ASCENDING,
+    DESCENDING
+
+}
