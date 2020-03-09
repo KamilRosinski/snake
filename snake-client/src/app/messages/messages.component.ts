7c9418e760335748647b1a0000c9917dedc6ab51
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-03-09 21:38:12 +0100

    Observables handled by async pipe

--- snake-client/src/app/messages/messages.component.ts
+++ snake-client/src/app/messages/messages.component.ts
@@ -1,35 +1,30 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Message} from './model/message.model';
 import {SortOrder} from './model/sort-order';
-import {Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
 import {Store} from '@ngrx/store';
 import {AppState} from '../store/state/app.state';
 import {selectAllMessages} from '../store/selectors/snake.selectors';
 import {clearMessages} from '../store/actions/snake.actions';
+import {map} from 'rxjs/operators';
 
 @Component({
     selector: 'app-messages',
     templateUrl: './messages.component.html',
     styleUrls: ['./messages.component.scss']
 })
-export class MessagesComponent implements OnInit, OnDestroy {
+export class MessagesComponent implements OnInit {
 
-    private readonly _subscription: Subscription = new Subscription();
-
-    private _messages: Message[] = [];
-    private _sortOrder: SortOrder = SortOrder.DESCENDING;
+    messages$: Observable<Message[]>;
+    hasMessages$: Observable<boolean>;
+    sortOrder: SortOrder = SortOrder.DESCENDING;
 
     constructor(private readonly _store: Store<AppState>) {
     }
 
     ngOnInit(): void {
-        this._subscription.add(this._store.select(selectAllMessages).subscribe((messages: Message[]) => {
-            this._messages = messages;
-        }));
-    }
-
-    ngOnDestroy(): void {
-        this._subscription.unsubscribe();
+        this.messages$ = this._store.select(selectAllMessages);
+        this.hasMessages$ = this.messages$.pipe(map((messages: Message[]) => messages.length > 0));
     }
 
     clearMessages(): void {
@@ -37,19 +32,11 @@ export class MessagesComponent implements OnInit, OnDestroy {
     }
 
     toggleSortOrder(): void {
-        this._sortOrder *= -1;
+        this.sortOrder *= -1;
     }
 
-    hasMessages(): boolean {
-        return this._messages && this._messages.length > 0;
-    }
-
-    isSortedAscending(): boolean {
-        return this._sortOrder === SortOrder.ASCENDING;
-    }
-
-    get messages(): Message[] {
-        return this._messages;
+    get arrowIcon(): string {
+        return this.sortOrder === SortOrder.ASCENDING ? 'arrow_upward' : 'arrow_downward';
     }
 
 }
--- snake-client/src/app/messages/pipe/reverse-order.pipe.ts
+++ snake-client/src/app/messages/pipe/reverse-order.pipe.ts
@@ -1,13 +1,14 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {Message} from '../model/message.model';
+import {SortOrder} from '../model/sort-order';
 
 @Pipe({
     name: 'reverseOrder'
 })
 export class ReverseOrderPipe implements PipeTransform {
 
-    transform(messages: Message[], reverseOrder: boolean): Message[] {
-        return reverseOrder ? [...messages].reverse() : messages;
+    transform(messages: Message[], sortOrder: SortOrder): Message[] {
+        return sortOrder === SortOrder.DESCENDING ? messages : [...messages].reverse();
     };
 
 }
