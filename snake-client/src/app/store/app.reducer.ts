f6479255cf524b600ea784cca27ef3243a7d539c
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-03-14 16:51:49 +0100

    Compilation fix for 'prod'

--- snake-client/src/app/store/app.reducer.ts
+++ snake-client/src/app/store/app.reducer.ts
@@ -1,4 +1,4 @@
-import {createReducer, on} from '@ngrx/store';
+import {Action, createReducer, on} from '@ngrx/store';
 import * as AppActions from './app.actions';
 import {AppState} from './app.state';
 import {GameStatus} from '../snake/shared/game-status';
@@ -22,36 +22,38 @@ const initialState: AppState = {
     }
 };
 
-export const appReducer = createReducer(
-    initialState,
-    on(AppActions.addMessageWithTimestamp, (state: AppState, action: { payload: Message }) => {
-        return {
-            ...state,
-            messages: [...state.messages, action.payload]
-        };
-    }),
-    on(AppActions.clearMessages, (state: AppState) => {
-        return {
-            ...state,
-            messages: []
-        };
-    }),
-    on(AppActions.updateGameStatus, (state: AppState, action: { payload: GameStatus }) => {
-        return {
-            ...state,
-            game: {
-                ...state.game,
-                status: action.payload
-            }
-        };
-    }),
-    on(AppActions.updateGameControls, (state: AppState, action: { payload: GameControls }) => {
-        return {
-            ...state,
-            game: {
-                ...state.game,
-                controls: action.payload
-            }
-        };
-    })
-);
+export function appReducer(state: AppState, action: Action): AppState {
+    return createReducer(
+        initialState,
+        on(AppActions.addMessageWithTimestamp, (state: AppState, action: { payload: Message }) => {
+            return {
+                ...state,
+                messages: [...state.messages, action.payload]
+            };
+        }),
+        on(AppActions.clearMessages, (state: AppState) => {
+            return {
+                ...state,
+                messages: []
+            };
+        }),
+        on(AppActions.updateGameStatus, (state: AppState, action: { payload: GameStatus }) => {
+            return {
+                ...state,
+                game: {
+                    ...state.game,
+                    status: action.payload
+                }
+            };
+        }),
+        on(AppActions.updateGameControls, (state: AppState, action: { payload: GameControls }) => {
+            return {
+                ...state,
+                game: {
+                    ...state.game,
+                    controls: action.payload
+                }
+            };
+        })
+    )(state, action);
+}
