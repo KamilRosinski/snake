57be24adfac51c5d08673b7e78fd40c3c88feba6
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-03-20 17:28:06 +0100

    Switch maps changed to concat maps

--- snake-client/src/app/store/app.effects.ts
+++ snake-client/src/app/store/app.effects.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {map, switchMap} from 'rxjs/operators';
+import {concatMap, map} from 'rxjs/operators';
 import * as AppActions from './app.actions';
 import {EvolutionService} from '../services/evolution.service';
 import {Evolution} from '../shared/evolution';
@@ -18,19 +18,19 @@ export class AppEffects {
 
     loadEvolutions$ = createEffect(() => this.actions$.pipe(
         ofType(AppActions.loadEvolutions),
-        switchMap(action => this.evolutionService.getAllEvolutions()),
+        concatMap(action => this.evolutionService.getAllEvolutions()),
         map((evolutions: Evolution[]) => AppActions.evolutionsLoaded({evolutions: evolutions}))
     ));
 
     createEvolution$ = createEffect(() => this.actions$.pipe(
         ofType(AppActions.createEvolution),
-        switchMap(action => this.evolutionService.createEvolution()),
+        concatMap(action => this.evolutionService.createEvolution()),
         map((evolution: Evolution) => AppActions.evolutionCreated({evolution}))
     ));
 
     deleteEvolution$ = createEffect(() => this.actions$.pipe(
        ofType(AppActions.deleteEvolution),
-       switchMap(action => this.evolutionService.deleteEvolution(action.evolutionId)),
+       concatMap(action => this.evolutionService.deleteEvolution(action.evolutionId)),
        map((evolutionId: number) => AppActions.evolutionDeleted({evolutionId}))
     ));
 
