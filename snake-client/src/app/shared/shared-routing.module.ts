c00231ec515a0756ac958be3105fdcfdaeea8642
Kamil Rosinski <kamilrosiczka@gmail.com>
2020-03-29 23:44:09 +0200

    Messages removed - FOREVER

--- snake-client/src/app/shared/shared-routing.module.ts
+++ snake-client/src/app/shared/shared-routing.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {MessagesComponent} from './components/messages/messages.component';
 import {MainMenuComponent} from './components/main-menu/main-menu.component';
 
 const routes: Routes = [
     {path: '', component: MainMenuComponent},
     {path: 'game', loadChildren: () => import('../game/game.module').then(m => m.GameModule)},
-    {path: 'evolution', loadChildren: () => import('../evolution/evolution.module').then(m => m.EvolutionModule)},
-    {path: 'messages', component: MessagesComponent}
+    {path: 'evolution', loadChildren: () => import('../evolution/evolution.module').then(m => m.EvolutionModule)}
 ];
 
 @NgModule({
--- snake-client/src/app/shared/shared.module.ts
+++ snake-client/src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MessagesComponent} from './components/messages/messages.component';
 import {MainMenuComponent} from './components/main-menu/main-menu.component';
 import {SharedRoutingModule} from './shared-routing.module';
 
 
 @NgModule({
     declarations: [
-        MainMenuComponent,
-        MessagesComponent
+        MainMenuComponent
     ],
     imports: [
         CommonModule,
